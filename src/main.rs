4c9084ce774b4153d7208b40bdb2ab0cce3e1079
René <steets@otech.nl>
2024-08-11 18:23:04 +0200

    Utils is too generic

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ use crate::prelude::*;
 use std::fs::read_dir;
 
 mod error;
+mod froms;
 mod prelude;
-mod utils;
 
 fn main() -> Result<()> {
-	println!("Hello, world!");
+    println!("Hello, world!");
 
-	for entry in read_dir("./")?.filter_map(|e| e.ok()) {
-		let entry: String = W(&entry).try_into()?;
-		println!("{entry}");
-	}
+    for entry in read_dir("./")?.filter_map(|e| e.ok()) {
+        let entry: String = W(&entry).try_into()?;
+        println!("{entry}");
+    }
 
-	Ok(())
+    Ok(())
 }
